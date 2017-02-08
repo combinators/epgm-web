0b60ab17943fca112716665d746f57601604ccba
kirankumarbs <kiranprofessional2@gmail.com>
2017-02-08 09:07:24 +0530

    created UI and Added Static Pages

--- app/dao/DocumentDbDao.scala
+++ app/dao/DocumentDbDao.scala
@@ -11,7 +11,7 @@ object  DocumentDbDao {
      val HOST = "https://epgm.documents.azure.com:443/";
      val MASTER_KEY = "JzmoDSMDC7BoTPzkA0QdeEyI4WJJSGDyaSH83n8yqckxkRRjHW8U8xJbJq7ivYEXaaGNzaIzvSUQg2tRZ06xfA==";
      val DATABASE_ID = "epgm-db"
-     val COLLECTION_ID = "log_data"
+     val COLLECTION_ID = "master_data"
 
     val documentClient = new DocumentClient(HOST,
       MASTER_KEY, ConnectionPolicy.GetDefault(),
@@ -41,12 +41,12 @@ object  DocumentDbDao {
 
 //    documentClient.deleteDocument(results.get(0).getSelfLink(), null);
 //
-//    val results1  = documentClient
-//      .queryDocuments(
-//        "dbs/" + DATABASE_ID + "/colls/" + COLLECTION_ID,
-//        "SELECT * FROM myCollection",
-//        null).getQueryIterable().toList();
-//    println("results1"+results1)
+    val results1  = documentClient
+      .queryDocuments(
+        "dbs/" + DATABASE_ID + "/colls/" + COLLECTION_ID,
+        "SELECT * FROM myCollection",
+        null).getQueryIterable().toList();
+    println("results1"+results1)
 
     //println("results1"+results)
 
