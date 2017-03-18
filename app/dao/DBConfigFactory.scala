c39ba3152fcd07a810609b48e06fd5b1a4556135
kirankumarbs <kiranprofessional2@gmail.com>
2017-03-18 16:23:20 +0530

    updated Docker image

--- app/dao/DBConfigFactory.scala
+++ app/dao/DBConfigFactory.scala
@@ -7,6 +7,7 @@ import com.microsoft.azure.documentdb.{ConnectionPolicy, ConsistencyLevel, Docum
   */
 object DBConfigFactory{
     def getHOST = "https://epgm.documents.azure.com:443/";
+    //
     def getMASTER_KEY = "0r8CYYVlo87KvsDjCipWlZtEBXWa2u2qEQWjTtd1ab0B2psDKHO6sceXsFgxKWiTZ1nUObIBknN3u2WnrWE4ig==";
     def getDATABASE_ID = "thewall"
     def getCOLLECTION_ID = "tyrion"
