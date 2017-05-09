ae8e5b55b52fa563d6c34f754f2ee43d858d8c37
kirankumarbs <kiranprofessional2@gmail.com>
2017-05-10 04:11:54 +0530

    updated db host

--- app/dao/DBConfigFactory.scala
+++ app/dao/DBConfigFactory.scala
@@ -6,9 +6,9 @@ import com.microsoft.azure.documentdb.{ConnectionPolicy, ConsistencyLevel, Docum
   * Created by kirankumarbs on 12/2/17.
   */
 object DBConfigFactory{
-    def getHOST = "https://epgmdb.documents.azure.com:443/";
+    def getHOST = "https://epgmdbs.documents.azure.com:443/";
     //
-    def masterKey = "TjNTMzB9lFaC6bIpu8kyTzrDGuQYjokMQhAVxEw9bAvoibuzCKnxW7RhYkBlWGkaq4b707Q63ldy0PIHhnyKng==";
+    def masterKey = "yBoqCjpYeTprTCI1hUtuyzAvLcqBclbibIHkpntNWC4vs1La2sWGS3ewapSpayGH4syAbdJK35pli89g1rUFZw==";
     def databaseId = "thewall"
     def collectionId = "tyrion"
     def documentClient = new DocumentClient(getHOST,
